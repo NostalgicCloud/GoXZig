a5474e292cf63db6f75932646d82879a2141c572
NostalgicCloud <retrocloudttv@gmail.com>
2025-03-30 15:14:05 -0500

    Added base Lua Interpretation for Zig and added dependent proj. Added Cint for go to id errors from Zig

--- build.zig
+++ build.zig
@@ -2,11 +2,18 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{
         .name = "ZigxGo",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
-        .optimize = b.standardOptimizeOption(.{}),
+        .optimize = optimize,
+    });
+    const lua_dep = b.dependency("zlua", .{
+        .target = target,
+        .optimize = optimize,
+        .lang = .lua54,
+        .shared = false,
     });
     exe.linkLibC();
     exe.addIncludePath(b.path("src"));
@@ -14,6 +21,7 @@ pub fn build(b: *std.Build) void {
     exe.linkSystemLibrary("pthread");
     //exe.addObjectFile(b.path("src/LIBS/main.a"));
     exe.addObjectFile(b.path("src/LIBS/server.a"));
+    exe.root_module.addImport("zlua", lua_dep.module("zlua"));
     b.installArtifact(exe);
 
     const run_step = b.step("run", "Run the app");
--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,36 @@
 const std = @import("std");
+const c = std.c;
 //const dprint = std.debug.print;
 const goMain = @cImport({
     @cInclude("headers/main.h");
     @cInclude("headers/server.h");
 });
+const zlua = @import("zlua");
+
+const Lua = zlua.Lua;
 pub fn main() !void {
     std.io.getStdOut().writeAll(
         "Hello World!\n",
     ) catch unreachable;
-    goMain.ServerInit(8090);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+    std.debug.print("ALLOCATORS ARE SET UP\n", .{});
+    const errCode: c_int = goMain.ServerInit(8090);
+
+    if (errCode != 1) {
+        std.debug.print("SERVER INIT ERROR: {}\n", .{errCode});
+        return error.ServerInitError;
+    }
     defer goMain.ServerClose();
+    std.debug.print("SERVER IS SET UP\n", .{});
+    // Initialize the Lua vm
+    var lua = try Lua.init(allocator);
+    defer lua.deinit();
+    std.debug.print("LUA IS SET UP\n", .{});
+    // Add an integer to the Lua stack and retrieve it
+    lua.pushInteger(42);
+    std.debug.print("{}\n", .{try lua.toInteger(1)});
 
     goMain.ReadInput();
 }
